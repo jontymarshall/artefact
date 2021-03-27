Corrected calculation of disc emission and scattering components
SED calculation fixes.

RT_Code.py
@@ -32,6 +32,8 @@ class RTModel:
     def __init__(self):
         print("Instantiated radiative transfer model object.")
         self.parameters = {}
+        self.sed_emit = 0.0
+        self.sed_scat = 0.0
         self.sed_disc = 0.0 
         self.sed_star = 0.0
         self.sed_wave = 0.0
@@ -437,12 +439,12 @@ class RTModel:
             for ij in range(0,int(self.parameters['nring'])):
                 scalefactor = scalefactor*self.scale[ij]/(2.*self.radii[ij]*au)**2
                 self.sed_rings[ij,:] = scalefactor * self.sed_star
-        
+                self.sed_scat += scalefactor * self.sed_star
         #convert model fluxes from flam to fnu (in mJy) 
         convertfactor = 1e3*1e26*(self.sed_wave*um)**2 /c
 
         self.sed_rings = self.sed_rings*convertfactor
-        self.sed_scat  = np.sum(self.sed_rings,axis=0)
+        self.sed_scat  = self.sed_scat*convertfactor
         self.sed_disc  += self.sed_scat   
 
     def calculate_dust_emission(self,blackbody=False,tolerance=0.01):
@@ -468,10 +470,10 @@ class RTModel:
                 scalefactor = self.ng[ii]*self.scale[ij]*((self.ag[ii]*um)**2)/(self.parameters['dstar']*pc)**2
                 tdust = RTModel.calculate_dust_temperature(self,self.radii[ij],qabs,blackbody=False,tolerance=0.01)
                 self.sed_ringe[ij,:] = scalefactor * qabs * np.pi * RTModel.planck_lam(self.sed_wave*um, tdust)
-        
+                self.sed_emit += scalefactor * qabs * np.pi * RTModel.planck_lam(self.sed_wave*um, tdust)
         #convert model fluxes from flam to fnu (in mJy) 
         convertfactor = 1e3*1e26*(self.sed_wave*um)**2 /c
 
         self.sed_ringe = self.sed_ringe*convertfactor
-        self.sed_emit  = np.sum(self.sed_ringe,axis=0)
+        self.sed_emit  = self.sed_emit*convertfactor
         self.sed_disc  += self.sed_emit*convertfactor